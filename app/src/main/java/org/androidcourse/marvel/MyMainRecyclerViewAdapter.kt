ec55e35cc446eaff4ac6d6e00188d02bd4e898cc
Lina <linamattsson@live.se>
2019-02-12 10:50:32 +0100

    click on start list to see more info, single character

--- app/src/main/java/org/androidcourse/marvel/MyMainRecyclerViewAdapter.kt
+++ app/src/main/java/org/androidcourse/marvel/MyMainRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package org.androidcourse.marvel
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -29,10 +30,7 @@ class MyMainRecyclerViewAdapter(
 
     init {
         mOnClickListener = View.OnClickListener { v ->
-            //val item = v.tag as DummyItem
-            // Notify the active callbacks interface (the activity, if the fragment is attached to
-            // one) that an item has been selected.
-            //mListener?.onListFragmentInteraction(item)
+
         }
     }
 
@@ -66,10 +64,17 @@ class MyMainRecyclerViewAdapter(
 //        val mContentView: TextView = mView.textView_all_character_description
         val mImageView: ImageView = mView.imageView_all_character_image
         val context = mView.context
-
+        init {
+            mView.setOnClickListener {
+                val intent = Intent(mView.context, CharacterDetailActivity::class.java)
+                intent.putExtra("characterId", item?.id)
+                mView.context.startActivity(intent)
+                println("clicked on character" + item?.name) }
+            }
+        }
 
 //        override fun toString(): String {
 //            return super.toString() + " '" + mContentView.text + "'"
 //        }
-    }
+
 }
